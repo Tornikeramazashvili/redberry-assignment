f7dde92e074df75e180ef6f3ae35a5fbde8ec9fd
albus <mnateus@gmail.com>
2023-02-07 15:45:14 +0400

    creating styles for the resume

--- src/pages/personalInformation/PersonalInformation.js
+++ src/pages/personalInformation/PersonalInformation.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react'
+import { useNavigate } from "react-router-dom";
 import { useForm } from 'react-hook-form'
 import './PersonalInformation.css'
 
 import checkMark from "../../assets/images/greenCheckmark.svg"
 import exclamationMark from '../../assets/images/redExclamationMark.svg'
+import arrowLeft from '../../assets/images/arrow-left.svg'
+import emailIcon from "../../assets/images/email.svg"
+import mobileIcon from "../../assets/images/mobile.svg"
 
 const PersonalInfoGeorgianString = "პირადი ინფო";
 const PersonalInfo = PersonalInfoGeorgianString.toUpperCase();
@@ -13,6 +17,7 @@ const button = buttonGeorgianString.toUpperCase();
 
 
 const PersonalInformation = () => {
+
   const { register, handleSubmit, watch, formState: { errors } } = useForm({
 
     defaultValues: {
@@ -25,6 +30,10 @@ const PersonalInformation = () => {
     }
   })
 
+  const onSubmit = data => {
+    console.log(data);
+  };
+
   const firstName = watch('firstName');
   const lastName = watch('lastName')
   const aboutMyself = watch('aboutMyself')
@@ -32,6 +41,9 @@ const PersonalInformation = () => {
   const mobile = watch('mobile')
   const image = watch('image')
 
+  const NAMES_REGEX = /^[ა-ჰ]+$/;
+  const EMAIL_REGEX = /^[a-zA-Z]+@redberry.ge$/;
+  const MOBILE_REGEX = /^\+995\d{9}$/;
 
   const [fileName, setFileName] = useState('');
 
@@ -39,14 +51,16 @@ const PersonalInformation = () => {
     setFileName(event.target.files[0].name);
   };
 
+  const navigateBack = useNavigate();
 
   return (
     <>
       <div className='container'>
+        <div className="arrow-left-container" >
+          <img src={arrowLeft} className='arrow-left' alt='arrow left logo' onClick={() => navigateBack(-1)} />
+        </div>
         <div className='form-container'>
-          <form onSubmit={handleSubmit((data) => {
-            console.log(data)
-          })}>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <div className='personal-information-container'>
               <div><span className='personal-information-title'>{PersonalInfo}</span></div>
               <div><span className='personal-information-pages'>1/3</span></div>
@@ -55,24 +69,24 @@ const PersonalInformation = () => {
               <div className='firstName-container'>
                 <span className={errors.firstName ? 'firstName-error-title' : 'firstName-default-title'}>სახელი</span>
                 <div className='firstname-input-and-error-container'>
-                  <input type='text' id='FIRST_NAME' className={firstName && !errors.firstName ? "firstName-success-input" : errors.firstName ? "firstName-error-input" : "firstName-default-input"}
-                    {...register('firstName', { required: true, minLength: 2, pattern: /^[ა-ჰ]+$/ })} placeholder='ანზორ' />
+                  <input type='text' id='FIRST_NAME' className={firstName && !errors.firstName && NAMES_REGEX.test(firstName) ? "firstName-success-input" : errors.firstName ? "firstName-error-input" : "firstName-default-input"}
+                    {...register('firstName', { required: true, minLength: 2, pattern: NAMES_REGEX })} placeholder='ანზორ' />
                   <span className='firstname-input-error'>
-                    {firstName && !errors.firstName ? (<img src={checkMark} alt='green checkmark' />) : errors.firstName ? (<img src={exclamationMark} alt='red exclamation mark' />) : ""}
+                    {firstName && !errors.firstName && NAMES_REGEX.test(firstName) ? (<img src={checkMark} alt='green checkmark' />) : errors.firstName ? (<img src={exclamationMark} alt='red exclamation mark' />) : ""}
                   </span>
                 </div>
                 <span className='input-hint'>მინიმუმ 2 ასო, ქართული ასოები</span>
               </div>
               <div className='lastName-container'>
-                <span className={errors.firstName ? 'lastName-error-title' : 'lastName-default-title'}>გვარი</span>
+                <span className={errors.lastName ? 'lastName-error-title' : 'lastName-default-title'}>გვარი</span>
                 <div className='lastname-input-and-error-container'>
                   <input
                     type='text'
                     id='LAST_NAME'
-                    className={lastName && !errors.lastName ? "lastName-success-input" : errors.lastName ? "lastName-error-input" : "lastName-default-input"}
-                    {...register('lastName', { required: true, minLength: 2, pattern: /^[ა-ჰ]+$/ })} placeholder='მუმლაძე' />
+                    className={lastName && !errors.lastName && NAMES_REGEX.test(lastName) ? "lastName-success-input" : errors.lastName ? "lastName-error-input" : "lastName-default-input"}
+                    {...register('lastName', { required: true, minLength: 2, pattern: NAMES_REGEX })} placeholder='მუმლაძე' />
                   <span className='lastname-input-error'>
-                    {lastName && !lastName.firstName ? (<img src={checkMark} alt='green checkmark' />) : errors.lastName ? (<img src={exclamationMark} alt='red exclamation mark' />) : ""}
+                    {lastName && !errors.lastName && NAMES_REGEX.test(lastName) ? (<img src={checkMark} alt='green checkmark' />) : errors.lastName ? (<img src={exclamationMark} alt='red exclamation mark' />) : ""}
                   </span>
                 </div>
                 <span className='input-hint'>მინიმუმ 2 ასო, ქართული ასოები</span>
@@ -103,12 +117,10 @@ const PersonalInformation = () => {
                 <input
                   type='email'
                   placeholder='anzorr666@redberry.ge'
-                  className={email && !errors.email ? "email-success-input" : errors.email ? "email-error-input" : "email-default-input "}
-                  {...register('email', {
-                    required: true, pattern: { value: /^[a-zA-Z]+@redberry.ge$/ }
-                  })} />
+                  className={email && !errors.email && EMAIL_REGEX.test(email) ? "email-success-input" : errors.email ? "email-error-input" : "email-default-input "}
+                  {...register('email', { required: true, pattern: EMAIL_REGEX })} />
                 <span className='input-error'>
-                  {email && !errors.email ? (<img src={checkMark} alt='green checkmark' />) : errors.email ? (<img src={exclamationMark} alt='red exclamation mark' />) : ""}
+                  {email && !errors.email && EMAIL_REGEX.test(email) ? (<img src={checkMark} alt='green checkmark' />) : errors.email ? (<img src={exclamationMark} alt='red exclamation mark' />) : ""}
                 </span>
               </div>
               <span className='input-hint'>უნდა მთავრდებოდეს @redberry.ge-ით</span>
@@ -119,20 +131,39 @@ const PersonalInformation = () => {
                 <input
                   placeholder='+995 551 12 34 56'
                   type='tel'
-                  className={mobile && !errors.mobile ? "mobile-success-input" : errors.mobile ? "mobile-error-input" : "mobile-default-input"}
-                  {...register('mobile', {
-                    required: true, pattern: { value: /^\+995\d{9}$/ }
-                  })} />
+                  className={mobile && !errors.mobile && MOBILE_REGEX.test(mobile) ? "mobile-success-input" : errors.mobile ? "mobile-error-input" : "mobile-default-input"}
+                  {...register('mobile', { required: true, pattern: MOBILE_REGEX })} />
                 <span className='input-error'>
-                  {mobile && !errors.mobile ? (<img src={checkMark} alt='green checkmark' />) : errors.mobile ? (<img src={exclamationMark} alt='red exclamation mark' />) : ""}
+                  {mobile && !errors.mobile && MOBILE_REGEX.test(mobile) ? (<img src={checkMark} alt='green checkmark' />) : errors.mobile ? (<img src={exclamationMark} alt='red exclamation mark' />) : ""}
                 </span>
               </div>
               <span className='input-hint'>უნდა აკმაყოფილებდეს ქართული მობილურის ნომრის ფორმატს</span>
             </div>
             <div className='button-container'>
-              <button className='button' type="submit">{button}</button>
+              <button className='button' type="submit">Submit</button>
             </div>
           </form>
+        </div>
+        <div>
+
+          <div className='second-form-container'>
+            <div className='second-form-firstname-lastname-container'>
+              <span className='second-form-firstname-title'>{firstName}</span>
+              <span className='second-form-lastname-title'>{lastName}</span>
+            </div>
+            <div className='second-form-email-container'>
+              {email ? <img src={emailIcon} alt='email' /> : ""}
+              <span className='second-form-email-title'>{email}</span>
+            </div>
+            <div className='second-form-mobile-container'>
+              {mobile ? <img src={mobileIcon} alt='email' /> : ""}
+              <span className='second-form-email-title'>{mobile}</span>
+            </div>
+            <div className='second-form-about-myself-container'>
+              {aboutMyself ? <span className='second-form-about-myself-title'>ჩემს შესახებ</span> : ""}
+              <p className='second-form-about-myself-text'>{aboutMyself}</p>
+            </div>
+          </div>
         </div>
       </div>
     </>
